1de8b7a125740aac07417fe2ad00f69f0f884e6f
umart94 <m_umar_t@hotmail.com>
2014-01-11 16:14:25 +0500

    Second

--- LoginForm/routes/user.js
+++ LoginForm/routes/user.js
@@ -4,5 +4,16 @@
  */
 
 exports.list = function(req, res){
-  res.send("respond with a resource");
+    var mongoose = require('mongoose');
+    var Schema      = mongoose.Schema;
+    var connection = mongoose.connect("mongodb://localhost/SELogin", function(err){
+        if(err){
+            console.log("Err");
+            res.send("a");
+        } else {
+            console.log("Connected To DB");
+            res.send("a");
+        }
+    });
+
 };
